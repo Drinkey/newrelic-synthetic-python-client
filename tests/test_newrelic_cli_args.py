f6d34df1020d200a312ec65596c1186e5d7e5864
Aaron Liang <aaron.liang@watchguard.com>
2022-11-22 17:19:14 +0800

    Adjust Scripted browser

--- tests/test_newrelic_cli_args.py
+++ tests/test_newrelic_cli_args.py
@@ -1,28 +1,19 @@
 from pathlib import PurePath, Path
-import pytest
 
 from src.newrelic.cli.args import ScriptedBrowserArguments
 
 
 def test_merge_content():
     script_browser = ScriptedBrowserArguments(
-        account_file=str(PurePath(Path(__file__).parent, "data", "account.js"))
+        script_content=[
+            str(PurePath(Path(__file__).parent, "data", "account.js")),
+            "Script_Content",
+        ]
     )
+
     assert script_browser.merge_content() == (
         "const FULL_URL = $secure.STAG_URL\\n"
         "const USERNAME = $secure.WSOM_STAG_DEU_USER\\n"
         "const PASSWORD = $secure.WSOM_STAG_DEU_PASSWD\\n"
-        "const SCRIPT_NAME"
+        "Script_Content"
     )
-
-
-def test_exception_not_found_account_file():
-    script_browser = ScriptedBrowserArguments(
-        account_file=str(
-            PurePath(Path(__file__).parent, "data", "accounts.js")
-        )
-    )
-
-    with pytest.raises(FileNotFoundError) as err:
-        script_browser.merge_content()
-    assert "Not found" in str(err)
